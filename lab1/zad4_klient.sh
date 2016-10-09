86fef5cba7aed500ded5b8890b765f4d037c2d39
Kamil Pek <kamil.pek@gmail.com>
2016-10-09 21:48:49 +0200

    poprawki zad4

--- lab1/zad4_klient.sh
+++ lab1/zad4_klient.sh
@@ -6,16 +6,16 @@ echo "podaj cyfre"
 
 read x
 
-echo $x > /home/majster/Dokumenty/wspolbiegi/lab1/zad4_dane.txt
+echo $x > zad4_dane.txt
 
 serwer_pid=`pidof zad4_serwer.sh`
 
 wait $serwer_pid
 
-wynik=$(cat /home/majster/Dokumenty/wspolbiegi/lab1/zad4_wyniki.txt)
+wynik=$(cat zad4_wyniki.txt)
 
 echo $wynik
 
-echo > /home/majster/Dokumenty/wspolbiegi/lab1/zad4_dane.txt
+echo > zad4_dane.txt
 
 exit
--- lab1/zad4_serwer.sh
+++ lab1/zad4_serwer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-plik="/home/majster/Dokumenty/wspolbiegi/lab1/zad4_dane.txt"
+plik="zad4_dane.txt"
 
 liczba=$(cat "$plik")
 
 let wynik=(liczba*liczba)/2
 
-echo $wynik > /home/majster/Dokumenty/wspolbiegi/lab1/zad4_wyniki.txt
+echo $wynik > zad4_wyniki.txt
 
 $0
