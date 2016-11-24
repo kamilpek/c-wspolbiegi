924fed913bbf204a4772b830c9a616bda1aa1328
Kamil Pek <kamil.pek@gmail.com>
2016-11-24 20:01:31 +0100

    lab4 gotowe

--- lab4/serwer.sh
+++ lab4/serwer.sh
@@ -13,30 +13,34 @@ fi
 digit='^[0-9]+$'
 valid='home'
 
-if [ ! -z $1 ] && [ ! -z $2 ]; then
-  klient="$1/Dokumenty/wspolbiegi/laborki/lab4/klientfifo"
-  if [[ -p $klient ]]; then
-    var=$2
-    let "var+=1"
-    echo "$var" >$klient
-  else
-    echo "nie mozna polaczyc z klientem"
-    echo $klient
-  fi
-fi
-
-if [[ -p $pipe ]]; then
-  while true
-  do
-    if read line <$pipe; then
-      copy=$line
-      line=$(sed 's/[0-9]*//g' <<< $line)
-      x=$line
-      copy=$(sed 's/[^0-9]//g' <<< $copy)
-      y=$copy
-      $0 $x $y &
+if [ ! -z $3 ]; then
+  if [ ! -z $1 ] && [ ! -z $2 ]; then
+    klient="$1/Dokumenty/wspolbiegi/laborki/lab4/klientfifo"
+    if [[ -p $klient ]]; then
+      var=$2
+      let "var+=1"
+      echo "$var" >$klient
+    else
+      echo "nie mozna polaczyc z klientem"
+      echo $klient
     fi
-done
-  else
-    echo "nima"
+  fi
+
+  if [[ -p $pipe ]]; then
+    while true
+    do
+      if read line <$pipe; then
+        copy=$line
+        line=$(sed 's/[0-9]*//g' <<< $line)
+        x=$line
+        copy=$(sed 's/[^0-9]//g' <<< $copy)
+        y=$copy
+        $0 $x $y &
+      fi
+  done
+    else
+      echo "nima"
+  fi
+else
+  $0 $1 $2 1 &
 fi
