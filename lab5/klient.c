22edc55543e9e9d11e246d9d4ae71e0e35580fff
Kamil Pek <kamil.pek@gmail.com>
2016-12-05 22:37:20 +0100

    lab5 postepy nieduze

--- lab5/klient.c
+++ lab5/klient.c
@@ -12,13 +12,25 @@ gcc klient.c -o klient.out -Wall
 #define SERWERFIFO "serwerfifo"
 #define KLIENTFIFO "klientfifo"
 
+int rozmiar(char komunikat[120]){
+  int index = 0;
+  int i = 0;
+  // int dlg = 0;
+  // dlg = sizeof(komunikat);
+  do{
+    index = index + 1;
+  } while(komunikat[i] != '\0');
+  printf("%d\t", index);
+  return index;
+}
+
 int main(int argc, char *argv[]){
   FILE *fp;
   char *home = getenv("HOME");
   int dlugosc = 0;
 
   if(argc != 2){
-    printf("Użycie: klientfifo [łanćuch]\n");
+    printf("Użycie: klientfifo [indeks rekordu bazy]\n");
     exit(1);
   }
 
@@ -32,11 +44,12 @@ int main(int argc, char *argv[]){
   strcat(komunikat, " ");
   strcat(komunikat, argv[1]);
 
-  // dlugosc = sizeof(komunikat);
+  dlugosc = sizeof(komunikat) / sizeof(komunikat[0]);
+  // dlugosc = rozmiar(komunikat);
 
-  // write(fp, "elo", 3);
   fputs(komunikat, fp);
-  printf("%d\n", dlugosc);
+  printf("%d\t", dlugosc);
+  printf("%c\n", komunikat[100]);
 
   fclose(fp);
   return(0);
