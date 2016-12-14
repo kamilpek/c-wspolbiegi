38c29c15f7dd5baefc4c5aa880724edc5e6ac75a
Kamil Pek <kamil.pek@gmail.com>
2016-12-14 16:53:40 +0100

    lab6 gotowe

---
+++ lab6/klient.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <string.h>
+#include <unistd.h>
+
+#define klucz 10
+
+typedef struct {
+   char polish[100];
+   char english[100];
+} dictionary;
+
+typedef struct {
+   long pID;
+   char slowo[100];
+} message;
+
+int main(int argc, char *argv[]) {
+
+  message communique;
+  int queveIn, queveOt;
+
+  queveIn=msgget(klucz, 0777);
+  queveOt=msgget(klucz, 0777);
+
+  strcpy(communique.slowo, argv[1]);
+  communique.pID=getpid();
+
+  if(msgsnd(queveIn, &communique, sizeof(char)*110, 0) == -1) {
+    printf("\nBłąd! Podczas wysyłania słowa do serwera");
+    exit(1); }
+
+  printf("J. polski: \"%s\" - ", argv[1]);
+
+  if(msgrcv(queveOt, &communique, sizeof(char)*110, getpid(), 0) == -1) {
+    printf("\nBłąd! Podczas odbierania słowa od serwera");
+    exit(2); }
+
+  printf("J. angielski: \"%s\".\n", communique.slowo);
+
+  return 0;
+}
---
+++ lab6/serwer.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
+#include <string.h>
+
+#define DICTSIZE 20
+#define klucz 10
+
+typedef struct {
+   char polish[100];
+   char english[100];
+} dictionary;
+
+typedef struct {
+   long pID;
+   char slowo[100];
+} message;
+
+dictionary dictPolEng[DICTSIZE] = {
+   {"rower", "bike"}, {"kot", "cat"},
+   {"pies", "dog"}, {"owca", "lamb"},
+   {"stół", "table"}, {"krzesło", "chair"},
+   {"mleko", "milk"}, {"zegar", "clock"},
+   {"prysznic", "shower"}, {"blisko", "near"},
+   {"dom", "house"}, {"samochód", "car"},
+   {"grać", "play"}, {"brać", "take"},
+   {"chwila", "while"}, {"drzewo", "tree"},
+   {"dzwonić", "call"}, {"głowa", "head"},
+   {"każdy", "each"}, {"który", "which"}
+};
+
+char *translate(char*);
+void endWork(int);
+void clQueve(int);
+
+int queveIn, queveOt;
+
+int main(int argc, char *argv[]) {
+
+   message communique;
+   queveIn = msgget(klucz, 0777 | IPC_CREAT);
+   queveOt = msgget(klucz, 0777 | IPC_CREAT);
+
+   while(1){
+      if(msgrcv(queveIn, &communique, sizeof(char)*110, 0, 0) == -1){
+         printf("Błąd podczas odbioru słowa od klienta.");
+         exit(1); }
+
+      printf("Odebrano: \"%s\", ", communique.slowo);
+      strcpy(communique.slowo, translate(communique.slowo));
+      printf("wysylanie do PID[%5ld] odpowiedz \"%s\".\n", communique.pID, communique.slowo);
+
+      if(msgsnd(queveOt, &communique, sizeof(char)*110, 0) == -1){
+         printf("\nBłąd! Przy wysyłaniu słowa do klienta");
+         exit(2); }
+   }
+   return 0;
+}
+
+char *translate(char* word){
+   int i;
+   for ( i = 0; i < DICTSIZE; i++) {
+      if(!strcmp(word, dictPolEng[i].polish))
+         return dictPolEng[i].english; }
+   return "Nie znam takiego slowa.";
+}
